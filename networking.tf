dc9a1907c69996dd92cf3e23087a2c0a34b21d39
ramg777 <ramganesh.palpandiraj@cloudreach.com>
2023-08-04 10:32:45 +0100

    adding networking file

---
+++ networking.tf
@@ -0,0 +1,41 @@
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "main-route-table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "route-table-association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.main-route-table.id
+}
+
+# Create a security group for the ECS tasks
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-security-group"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
